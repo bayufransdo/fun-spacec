c6dd8454bb6f4129a119fee3c9e49c8d9f8b041c
Bayu Maulana <bayufransdo@gmail.com>
2024-04-01 19:44:09 +0700

    First commit

---
+++ app.py
@@ -0,0 +1,8 @@
+def luas_segitiga():
+    a = int(input("Masukkan alas segitiga: "))
+    t = int(input("Masukkan tinggi segitiga: "))
+    luas = 0.5 * a * t
+    print("Luas segitiga adalah", luas)
+
+
+luas_segitiga()
